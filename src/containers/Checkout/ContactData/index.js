413684c7ceb1ed97b84c2dd199896afd6683d26c
Oleh Kyba <vestigor0@gmail.com>
2020-08-01 15:07:06 +0300

    Contact Data component

---
+++ src/containers/Checkout/ContactData/index.js
@@ -0,0 +1,75 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+
+import axios from "../../../utils/axios/builder";
+
+import classes from "./ContactData.module.css";
+
+import Button from "../../../components/UI/Button";
+import Spinner from "../../../components/UI/Spinner";
+
+class ContactData extends Component {
+
+    state = {
+        name: 'Oleh',
+        email: 'vestigor0@gmail.com',
+        address: {
+            street: 'Ольжича 35А',
+            postalCode: '04060',
+        },
+        isLoading: false,
+    };
+
+    onSubmit = event => {
+        event.preventDefault();
+        const {isLoading, ...customer} = this.state;
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer,
+        };
+        this.setState({isLoading: true});
+        axios.post("/orders/", order)
+            .then(() => {
+                this.setState({isLoading: false});
+                const path = this.props.match.url + '/success';
+                this.props.history.replace(path);
+            })
+            .catch(error => {
+                this.setState({isLoading: false});
+                const path = this.props.match.url + '/error?message=' + error.message;
+                this.props.history.replace(path);
+            });
+    };
+
+    render() {
+        return (
+            <Spinner isSpin={this.state.isLoading}>
+                <div className={classes.Container}>
+                    <h4 className={classes.Title}>Enter your contact data:</h4>
+                    <form className={classes.FormContainer}>
+                        <input type="text" placeholder="Your Name"/>
+                        <input type="email" placeholder="Email"/>
+                        <input type="text" placeholder="Street"/>
+                        <input type="text" placeholder="Post Code"/>
+                        <div className={classes.ButtonsContainer}>
+                            <Button width={'80px'} onClick={this.onSubmit}>Confirm</Button>
+                            {this.props.onCancel && <Button width={'80px'} onClick={this.props.onCancel} invert>Cancel</Button>}
+                        </div>
+                    </form>
+                </div>
+            </Spinner>
+        );
+    }
+}
+
+ContactData.propTypes = {
+    ingredients: PropTypes.arrayOf(PropTypes.shape({
+        type: PropTypes.string,
+        count: PropTypes.number,
+    })),
+    price: PropTypes.number,
+    onCancel: PropTypes.func,
+};
+
+export default ContactData;
--- src/containers/Checkout/index.js
+++ src/containers/Checkout/index.js
@@ -3,6 +3,8 @@ import {Switch, Route, withRouter} from "react-router-dom";
 
 import classes from "./Checkout.module.css";
 
+import ContactData from "./ContactData";
+
 import Card from "../../components/UI/Card";
 import OrderSummary from "../../components/OrderSummary";
 
@@ -11,6 +13,11 @@ class Checkout extends Component {
     state = {
         ingredients: [],
         price: 0,
+    };
+
+    onCheckoutCancel = event => {
+        event.preventDefault();
+        this.props.history.goBack();
     };
 
     componentDidMount() {
@@ -28,13 +35,24 @@ class Checkout extends Component {
     }
 
     render() {
+        console.log(this.props.history)
         return (
             <section className={classes.Container}>
                 <Card>
                     <OrderSummary menu={this.state.ingredients} price={this.state.price}/>
                 </Card>
                 <Switch>
-                    <Route exact path={this.props.match.url + "/"} render={() => <p>Form</p>}/>
+                    <Route
+                        exact
+                        path={this.props.match.url + "/"}
+                        render={props => (
+                            <ContactData
+                                {...props}
+                                onCancel={this.onCheckoutCancel}
+                                ingredients={this.state.ingredients}
+                                price={this.state.price}
+                            />)}
+                    />
                     <Route path={this.props.match.url + "/:status"} render={props => <p>{props.match.params.status}</p>}/>
                 </Switch>
             </section>
